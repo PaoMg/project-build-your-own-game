b450b5d5fe2cd155d18244edd881279c5783cf15
Miguel Campero <mikecviteri@icloud.com>
2020-10-24 04:13:08 -0500

    agregando nuevas funciones

--- guess_who.py
+++ guess_who.py
@@ -1,108 +1,177 @@
 import random
 import json
 
-with open('./data.json') as f:
-  diccionario_personajes = json.load(f)
+def game():
+    with open('./data.json') as f:
+        diccionario_personajes = json.load(f)
 
-# Filtrando atributos
-personajes = [name for name in diccionario_personajes]
-all_casas = list(dict.fromkeys([diccionario_personajes[i]['casa'] for i in personajes]))
-all_patronus = list(dict.fromkeys([diccionario_personajes[i]['patronus'] for i in personajes]))
-all_generos = list(dict.fromkeys([diccionario_personajes[i]['genero'] for i in personajes]))
-personajes_1 = []
+    # Filtrando keys
+    personajes = [name for name in diccionario_personajes]
+    all_casas = list(dict.fromkeys([diccionario_personajes[i]['casa'] for i in personajes]))
+    all_patronus = list(dict.fromkeys([diccionario_personajes[i]['patronus'] for i in personajes]))
+    all_generos = list(dict.fromkeys([diccionario_personajes[i]['genero'] for i in personajes]))
 
-# Selección de personaje aleatorio
-secreto = random.choice(list(diccionario_personajes.keys()))
+    # Selección de personaje aleatorio
+    secreto = random.choice(list(diccionario_personajes.keys()))
 
-# Funciones a evaluar
-def adivinar_per(personajes, secreto):
-    print('\nOpciones:', personajes)
-    if input() == secreto:
-        print('¡Felicidades, ganaste el juego!')
-        exit()
-    else:
-        print('Personaje incorrecto')
+    # Funciones a evaluar
+    def adivinar_per(personajes, secreto):
+        print('\nOpciones:', menos_errados)
+        ans = False
+        while ans == False:
+            guess = input()
+            if guess.lower() in [personajes.lower() for personajes in personajes]:
+                if guess.lower() == secreto.lower():
+                    while True:
+                        juego = input("Felicidades! Ganaste! :) Quieres jugar de nuevo? Y/N")
+                        if juego.lower() == 'y':
+                            game()
+                        if juego.lower() == 'n':
+                            print("Gracias por jugar")
+                            exit()
+                else:
+                    print('\nPersonaje incorrecto')
+                    [pers_err.append(guess.capitalize())]
+                    return posibles
+                    ans = True
+            else:
+                print("Por favor selecciona un personaje correcto")
+
+    def adivinar_casa(personajes, casa):
+        print('\nOpciones:', all_casas)
+        ans = False
+        while ans == False:
+            guess = input()
+            if guess.lower() in [all_casas.lower() for all_casas in all_casas]:
+                if guess.lower() == casa.lower():
+                    print('\nCasa correcta')
+                    personajes = [pers for pers in personajes if diccionario_personajes[pers]['casa'] == casa]
+                    [correctas.append(3)]
+                    return personajes
+                    ans = True
+                else:
+                    print('\nCasa erronea')
+                    return posibles
+                    ans = True
+            else:
+                print("Por favor selecciona una casa correcta")
+
+    def adivinar_patronus(personajes, patronus):
+        print('\nOpciones:', all_patronus)
+        ans = False
+        while ans == False:
+            guess = input()
+            if guess.lower() in [all_patronus.lower() for all_patronus in all_patronus]:
+                if guess.lower() == patronus.lower():
+                    print('\nPatronus correcto')
+                    personajes = ([pers for pers in personajes if diccionario_personajes[pers]['patronus'] == patronus])
+                    [correctas.append(4)]
+                    return personajes
+                    ans = True
+                else:
+                    print('\nPatronus erróneo')
+                    return posibles
+                    ans = True
+            else:
+                print("Por favor selecciona un patronus correcto")
+
+
+    def adivinar_genero(personajes, genero):
+        print('\nOpciones:', all_generos)
+        ans = False
+        while ans == False:
+            guess = input()
+            if guess.lower() in [all_generos.lower() for all_generos in all_generos]:
+                if guess.lower() == genero.lower():
+                    print('\nGénero correcto')
+                    personajes = [pers for pers in personajes if diccionario_personajes[pers]['genero'] == genero]
+                    [correctas.append(2)]
+                    return personajes
+                    ans = True
+                else:
+                    print('\nGénero incorrecto')
+                    return posibles
+                    ans = True
+            else:
+                print("Por favor selecciona un género correcto")
+
+
+    def unir_listas(lista_a, lista_b):
+        descartados = [i for i in lista_a if i not in lista_b]
+        [blacklist.append(i) for i in descartados]
         return posibles
 
-def adivinar_casa(personajes, casa):
-    print('\nOpciones:', all_casas)
-    print('Elige la casa del personaje')
-    if input() == casa:
-        print('Casa correcta')
-        personajes = [pers for pers in personajes if diccionario_personajes[pers]['casa'] == casa]
-        return personajes
-    else:
-        print('Casa erronea')
-        return posibles
+
+    # Iniciar el juego
+    tries = 0
+    posibles = personajes[:]
+    blacklist = []
+    preguntas = 4
+    correctas = []
+    pers_err = []
+
+    while True:
+       rondas = input("Cuántas rondas quieres jugar?")
+       try:
+           rondas = int(rondas)
+       except ValueError:
+           print ("Por favor ingresa un número correcto")
+       else:
+           break
 
 
-def adivinar_patronus(personajes, patronus):
-    print('\nOpciones:', all_patronus)
-    print('Elige el patronus del personaje')
-    if input() == patronus:
-        print('Patronus correcto')
-        personajes = ([pers for pers in personajes if diccionario_personajes[pers]['patronus'] == patronus])
-        return personajes
-    else:
-        print('Patronus erroneo')
-        return posibles
+    print('\nInicia el juego.')
 
+    while tries < rondas:
+        print(f"\nIntento: {tries+1}/{rondas}")
+        set(blacklist)
+        opciones = [i for i in [i + 1 for i in range(preguntas)] if i not in correctas]
+        lista_posibles = [x for x in posibles if x not in blacklist]
+        menos_errados = [i for i in lista_posibles if i not in pers_err]
 
-def adivinar_genero(personajes, genero):
-    print('\nOpciones:', all_generos)
-    print('Elige el género del personaje')
-    if input() == genero:
-        print('Genero correcto')
-        personajes = [pers for pers in personajes if diccionario_personajes[pers]['genero'] == genero]
-        return personajes
-    else:
-        print('Genero incorrecto')
-        return posibles
+        print("\nPersonajes posibles: ", menos_errados)
+        try:
+            if tries == rondas - 1:
+                print("\nÚltimo intento! Adivina el personaje")
+                eleccion = 1
+            else:
+                print(f"Elige un número: ({', '.join(str(e) for e in opciones)})")
+                print('1. Adivinar el personaje')
+                if 2 not in correctas:
+                    print('2. Adivinar su género')
+                if 3 not in correctas:
+                    print('3. Adivinar su casa')
+                if 4 not in correctas:
+                    print('4. Adivinar su patronus')
+                eleccion = int(input())
 
+            if eleccion == 1:
+                adivinar_per(personajes, secreto)
+            elif eleccion == 2 and 2 not in correctas:
+                personajes_nuevos = adivinar_genero(posibles, diccionario_personajes[secreto]['genero'])
+                personajes_nuevos = unir_listas(posibles, personajes_nuevos)
+            elif eleccion == 3 and 3 not in correctas:
+                personajes_nuevos = adivinar_casa(posibles, diccionario_personajes[secreto]['casa'])
+                personajes_nuevos = unir_listas(posibles, personajes_nuevos)
+            elif eleccion == 4 and 4 not in correctas:
+                personajes_nuevos = adivinar_patronus(posibles, diccionario_personajes[secreto]['patronus'])
+                personajes_nuevos = unir_listas(posibles, personajes_nuevos)
+            elif eleccion in correctas:
+                print("Ese valor ya lo elegiste antes")
+                tries -= 1
+            else:
+                raise ValueError
+            tries += 1
+        except ValueError:
+            print('Oops! Esa no fue una opción correcta. Intentémoslo de nuevo...')
 
-def unir_listas(lista_a, lista_b):
-    # print("lista a: ", lista_a)
-    # print("lista b: ", lista_b)
-    descartados = [i for i in lista_a if i not in lista_b]
-    [blacklist.append(i) for i in descartados]
-    return posibles
+    # Pierde el juego
+    while True:
+        juego = input(f"Lo siento, perdiste. El personaje secreto era {secreto} :( Quieres jugar de nuevo? Y/N")
+        if juego.lower() == 'y':
+            game()
+        if juego.lower() == 'n':
+            print("Gracias por jugar")
+            exit()
 
-
-# Inicia el juego
-win = False
-tries = 0
-posibles = personajes[:]
-blacklist = []
-print('Inicia el juego. Tienes 5 intentos para adivinar el personaje.')
-
-while tries < 5 or win == False:
-    print('\nIntentos:', tries)
-    set(blacklist)
-    print("Personajes posibles: ", [x for x in posibles if x not in blacklist])
-    try:
-        print('Elige un número (1,2,3 4)\n')
-        print('1. Adivinar el personaje')
-        print('2. Adivinar su género')
-        print('3. Adivinar su casa')
-        print('4. Adivinar su patronus')
-        eleccion = int(input())
-        if eleccion == 1:
-            adivinar_per(personajes, secreto)
-        elif eleccion == 2:
-            personajes_nuevos = adivinar_genero(posibles, diccionario_personajes[secreto]['genero'])
-            personajes_nuevos = unir_listas(posibles, personajes_nuevos)
-        elif eleccion == 3:
-            personajes_nuevos = adivinar_casa(posibles, diccionario_personajes[secreto]['casa'])
-            personajes_nuevos = unir_listas(posibles, personajes_nuevos)
-        elif eleccion == 4:
-            personajes_nuevos = adivinar_patronus(posibles, diccionario_personajes[secreto]['patronus'])
-            personajes_nuevos = unir_listas(posibles, personajes_nuevos)
-        else:
-            raise ValueError
-        tries += 1
-    except ValueError:
-        print('Oops!  Eso no fue una opción correcta. Intentémoslo de nuevo...')
-
-# Pierde el juego
-print('Lo siento, perdiste :(')
-print('Fin del juego')
+game()
